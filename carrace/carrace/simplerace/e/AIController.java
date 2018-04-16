52e43de199a06b7d702ab71353c4ab8dff357e36
Shinjirow <g1644204@cc.kyoto-su.ac.jp>
2018-04-17 02:45:41 +0900

    update

--- carrace/carrace/simplerace/e/AIController.java
+++ carrace/carrace/simplerace/e/AIController.java
@@ -3,15 +3,24 @@ import simplerace.*;
 
 public class AIController implements Controller, Constants {
 
+    private boolean DEBUG = false;
+
     private int status = 0;
 
     private double targetAngle;
 
-    private Driver aDriver = new Driver();
+    private Driver aDriver = new Driver(this);
 
     public void reset() {}
 
+    public void transition(){
+        this.status++;
+        if(this.status == 4) this.status = 0;
+        return;
+    }
+
     public int control (SensorModel inputs) {
+        if(DEBUG) System.out.println(status);
 
         this.targetAngle = inputs.getAngleToNextWaypoint();
 
@@ -30,33 +39,90 @@ public class AIController implements Controller, Constants {
             command = this.aDriver.decelerate(inputs);
         }else if(this.status == 3){
             command = this.aDriver.turn(inputs);
-        }else if(this.status == 4){
-            command = this.aDriver.adjust(inputs);
         }
 //		System.out.print(command);
         return command;
     }
 }
 
-class Driver extends Object {
+class Driver extends Object implements Constants {
 
-    public Driver(){
+    private boolean DEBUG = false;
+
+    private Vector2d currentTarget = null;
+
+    private AIController aiController;
+
+    public Driver(AIController aiC){
+        this.aiController=aiC;
         return;
     }
 
+    public void setTarget(Vector2d target){
+        this.currentTarget = target;
+        return;
+    }
+
+    //---controls----
+
     public int accelerate(SensorModel inputs){
-        return 0;
+        //if(DEBUG) System.out.println("distance = "+inputs.getDistanceToNextWaypoint());
+
+        if(inputs.getDistanceToNextWaypoint()<0.25) aiController.transition();
+
+        int cmd=forward;
+        if(inputs.getAngleToNextWaypoint()< -0.1){
+            cmd=forwardright;
+        }else if(inputs.getAngleToNextWaypoint() > 0.1){
+            cmd=forwardleft;
+        }
+        return cmd;
     }
 
     public int decelerate(SensorModel inputs){
-        return 0;
+        if(inputs.getSpeed() < 2.3) aiController.transition();
+
+        int cmd=backward;
+        return cmd;
     }
 
     public int turn(SensorModel inputs){
-        return 0;
+        if(this.currentTarget==null) this.setTarget(inputs.getNextWaypointPosition());
+        if(inputs.getNextWaypointPosition() != this.currentTarget) {
+            aiController.transition();
+            this.setTarget(inputs.getNextWaypointPosition());
+        }
+
+        int cmd=neutral;
+        if(inputs.getAngleToNextWaypoint()<0.0){
+            cmd=forwardright;
+        }else{
+            cmd=forwardleft;
+        }
+        return cmd;
     }
 
     public int adjust(SensorModel inputs){
-        return 0;
+        if(inputs.getAngleToNextWaypoint() < 0.1 && inputs.getAngleToNextWaypoint() > -0.1) aiController.transition();
+
+        if(DEBUG) System.out.println("angle = " + inputs.getAngleToNextWaypoint() + ", distance = " + inputs.getDistanceToNextWaypoint());
+
+        int cmd=neutral;
+        if(inputs.getAngleToNextWaypoint()<0.0){
+            if(inputs.getSpeed() > 1.8)
+                cmd=right;
+            else
+                cmd=forwardright;
+        }else{
+            if(inputs.getSpeed() > 1.8)
+                cmd=left;
+            else
+                cmd=forwardleft;
+        }
+
+        if(inputs.getAngleToNextWaypoint() > 1.3 && inputs.getDistanceToNextWaypoint() < 0.07) cmd = forward;
+        if(inputs.getAngleToNextWaypoint() < -1.3 && inputs.getDistanceToNextWaypoint() < 0.07) cmd = forward;
+
+        return cmd;
     }
 }
