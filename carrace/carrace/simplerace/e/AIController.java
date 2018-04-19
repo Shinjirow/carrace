32de7aa48d09e0d0595fcf64f1970867df67872b
Shinjirow <g1644204@cc.kyoto-su.ac.jp>
2018-04-19 10:42:10 +0900

    update

--- carrace/carrace/simplerace/e/AIController.java
+++ carrace/carrace/simplerace/e/AIController.java
@@ -4,28 +4,14 @@ import simplerace.*;
 public class AIController implements Controller, Constants {
 
     private double targetAngle;
-
-    private int stats = 0;
-
-    long fl=0;
-
+    
     public void reset(){}
 
     public int control (SensorModel inputs) {
 
-        System.out.println("f = "+ fl + ", Speed = " + inputs.getSpeed());
+        System.out.println("Speed = " + inputs.getSpeed());
 
-        int command=neutral;
-
-        if(stats==0){
-            command=forward;
-            if(inputs.getSpeed()>10.0) this.stats=1;
-        }else if(stats == 1){
-            command=backward;
-            if(inputs.getSpeed()<0.0) this.stats=2;
-        }
-
-        fl++;
+        int command=backwardleft;
 
         return command;
     }
