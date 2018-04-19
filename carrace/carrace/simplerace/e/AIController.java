00c051aa16d8d0519f4eacd0e73e50a6c1f5820b
Shinjirow <g1644204@cc.kyoto-su.ac.jp>
2018-04-19 12:54:49 +0900

    減速距離のテスト

--- carrace/carrace/simplerace/e/AIController.java
+++ carrace/carrace/simplerace/e/AIController.java
@@ -4,14 +4,24 @@ import simplerace.*;
 public class AIController implements Controller, Constants {
 
     private double targetAngle;
-    
+
+    boolean flag = false;
+
+    Vector2d startpos;
+
     public void reset(){}
 
     public int control (SensorModel inputs) {
 
         System.out.println("Speed = " + inputs.getSpeed());
 
-        int command=backwardleft;
+        int command=backward;
+
+        if(!flag) if(inputs.getSpeed() < -10) {flag = true;startpos = inputs.getPosition();}
+
+        if(flag) if(inputs.getSpeed() < 0)command = forward;else command = neutral;
+
+        System.out.println("position = " + inputs.getPosition());
 
         return command;
     }
