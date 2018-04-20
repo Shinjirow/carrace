e4700188a304f3b67678ddd58f6915a2af8cdf46
Shinjirow <g1644204@cc.kyoto-su.ac.jp>
2018-04-20 16:46:43 +0900

    analyze.py更新

--- carrace/carrace/analyze.py
+++ carrace/carrace/analyze.py
@@ -2,10 +2,18 @@ sum = 0
 
 num = 10000
 
+mx = -100
+
+mn = 114514
+
+
 for i in range(num):
 	n = input()
 	sum = sum + n
-
+	mx = max(n, mx)
+	mn = min(n, mn)
 
 ave = float(sum) / float(num)
 print('10000 try average = ' + str(ave))
+print('max = ' + str(mx))
+print('min = ' + str(mn))
