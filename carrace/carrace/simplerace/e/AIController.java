363d8f23633b808d13d8b3e96e911d6018e4c884
Shinjirow <g1644204@cc.kyoto-su.ac.jp>
2018-04-25 23:40:54 +0900

    min0対策が生きてるかテスト

--- carrace/carrace/simplerace/e/AIController.java
+++ carrace/carrace/simplerace/e/AIController.java
@@ -9,20 +9,43 @@ public class AIController implements Controller, Constants {
 
     public void reset(){}
 
+    private boolean isTooClose(SensorModel inputs){
+        double mergin = 12.5 + 20.0;
+        //System.err.println(inputs.getDistanceToNextWaypoint()*Math.sqrt(320000.0D) < mergin && Math.abs(inputs.getAngleToNextWaypoint()) < Math.PI*0.6);
+        
+        return (inputs.getDistanceToNextWaypoint()*Math.sqrt(320000.0D) < mergin && Math.abs(inputs.getAngleToNextWaypoint()) < Math.PI*0.8) ? true : false;
+    }
+
+    /**
+     * reverseLR
+     * コマンドの左右を反転させる
+     */
+    private int reverseLR(int cmd){
+        int command = cmd;
+        if(cmd == forwardleft)        command = forwardright;
+        else if(cmd == forwardright)  command = forwardleft;
+        else if(cmd == backwardright) command = backwardleft;
+        else if(cmd == backwardleft)  command = backwardright;
+        else if(cmd == left)          command = right;
+        else if(cmd == right)         command = left;
+        return command;
+    }
+
     public int control (SensorModel inputs) {
 
         int command = backwardleft;
 
-        System.out.println(inputs.getDistanceToNextWaypoint()*Math.sqrt(32000.0D));
+        //System.out.println(inputs.getDistanceToNextWaypoint()*Math.sqrt(32000.0D));
 
 
-        /**
+        
         if(inputs.getAngleToNextWaypoint() > 0){
             command = backwardleft;
         }else{
             command = backwardright;
         }
-         */
+         
+        if(this.isTooClose(inputs)) command = reverseLR(command);
 
         f++;
 
